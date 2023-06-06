9bb72734230b5593d9e434dff35107f25360d7af
Dinamow <meemoo102039@gmail.com>
2023-06-06 11:08:56 +0000

    final fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
+if number < 0:
+    digit = number % -10
+else:
+    digit = number % 10
 print(f"Last digit of {number:d} is {digit:d} and", end = ' ')
 if digit == 0:
     print("is 0")
