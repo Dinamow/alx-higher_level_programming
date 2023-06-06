5ab1f03f524081b3cb114f355e2fedeb31816711
Dinamow <meemoo102039@gmail.com>
2023-06-06 11:44:52 +0000

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i, 10):
-        print("{:d}{:d}".format(i, j), end=' ')
+        if j == 0:
+            pass
+        else:
+            print("{:d}{:d}".format(i, j), end=' ')
 print("{:d}".format(89))
