ed2a2877f1e7f12f0b77c7cf744ce701d773e421
Dinamow <meemoo102039@gmail.com>
2023-06-08 18:46:29 +0000

    add argv

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lenth = len(sys.argv)
+    i, loop = 0, 0
+
+    if lenth == 1:
+        print("{:d}".format(0))
+    else:
+        while (loop < lenth - 1):
+            loop += 1
+            i += int(sys.argv[loop])
+        print("{:d}".format(i))
+            
+
