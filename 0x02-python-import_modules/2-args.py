ab1317c86d3f55439d75492448fc27b02fd2962b
Dinamow <meemoo102039@gmail.com>
2023-06-08 18:32:43 +0000

    another fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-i = 1
-if (len(sys.argv) == 1):
-    print("0 arguments.")
-elif (len(sys.argv) == 2):
-    print("1 argument:\n1: {:s}".format(sys.argv[1]))
-else:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-    while (i <= len(sys.argv) - 1):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
-        i += 1
+if __name__ == "__main__":
+    import sys
+    i = 1
+    if (len(sys.argv) == 1):
+        print("0 arguments.")
+    elif (len(sys.argv) == 2):
+        print("1 argument:\n1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        while (i <= len(sys.argv) - 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
