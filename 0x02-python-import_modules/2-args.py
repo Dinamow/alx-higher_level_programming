00e99400fc31664578bc5de373dd1980aa115920
Dinamow <meemoo102039@gmail.com>
2023-06-08 18:26:46 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ import sys
 i = 1
 if (len(sys.argv) == 1):
     print("0 arguments.")
+elif (len(sys.argv) == 2):
+    print("1 argument:\n1: {:s}".format(sys.argv[1]))
 else:
-    print("{:d} argument:".format(len(sys.argv) - 1))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
     while (i <= len(sys.argv) - 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
         i += 1
