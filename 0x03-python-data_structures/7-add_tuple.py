7e685a7fa3056b83cb2a0b9bd7541977b6152f77
Dinamow <meemoo102039@gmail.com>
2023-06-10 11:57:01 +0000

    alot

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,4 @@
+#!/use/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = 0
+    for x :
---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+list_a = [1, 2, 3]
+print(list_a)
+tuple_a = list_a,
+print(tuple_a)
+print("type of tuple_a is {}".format(type(tuple_a)))
