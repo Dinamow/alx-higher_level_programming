20afd2c20b9173bdb61b0202f617f8cb6577fcbd
DINAMOW <meemoo102039@gmail.com>
2023-06-14 21:35:58 +0000

    final

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new = a_dictionary.copy()
     for x in a_dictionary:
-       a_dictionary[x] = a_dictionary.get(x) * 2
-    return a_dictionary
+       new[x] = a_dictionary.get(x) * 2
+    return new
