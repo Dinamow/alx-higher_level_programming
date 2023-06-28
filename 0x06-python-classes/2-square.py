8188c6a668534fb3734a17518b69031aa7544adf
DINAMOW <meemoo102039@gmail.com>
2023-06-28 14:53:57 +0000

    finally

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,10 +5,9 @@
 class Square:
     """here the square class"""
     def __init__(self, size=0):
-        try:
-            int(size)
-        except ValueError:
-            raise TypeError("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is class file"""
+
+
+class Square:
+    """this is the suqare class"""
+    def __init__(self, size=0):
+
+   
