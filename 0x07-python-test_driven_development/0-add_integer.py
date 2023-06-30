039a2d4b31237adb4782264563097c6b480348c9
DINAMOW <meemoo102039@gmail.com>
2023-06-30 23:41:25 +0000

    1st done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+this file for functions
+
+add_integer
+"""
+
 
 def add_integer(a, b=98):
-    if not isinstance(a, int) and not isinstance(a, float):
+    """
+    adding 2 integers
+    """
+    if not isinstance(a, int) and not isinstance(a, float) or isinstance(a, bool):
         raise TypeError('a must be an integer')
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float) or isinstance(b, bool):
         raise TypeError('b must be an integer')
-    
+
     return int(a) + int(b)
