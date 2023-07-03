7b2132e6c824be5fd98d73f25febd605750ff0b2
DINAMOW <meemoo102039@gmail.com>
2023-07-03 10:26:24 +0000

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,18 +5,16 @@
 class Rectangle(object):
     """this is rectangle class"""
     def __init__(self, width=0, height=0):
-        try:
-            self.__width = int(width)
-            if self.__width < 0:
-                raise ValueError('width must be >= 0')
-        except TypeError:
+        if not isinstance(width, int) or isinstance(width, bool):
             raise TypeError('width must be an integer')
-        try:
-            self.__height = int(height)
-            if self.__height < 0:
-                raise ValueError('height must be >= 0')
-        except TypeError:
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if not isinstance(height, int) or isinstance(height, bool):
             raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
