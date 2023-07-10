f8c7f08c6fa4ca5eafe2648d70299a4065df5299
DINAMOW <meemoo102039@gmail.com>
2023-07-10 19:20:11 +0000

    test

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is module"""
+
+
+class MyList(list):
+    """this is calss"""
+    def print_sorted(self):
+        """this func type sorted list"""
+        return print(sorted(self))
