5fb95d611e343a1b2e1530aaa28308eb7ca19fe5
DINAMOW <meemoo102039@gmail.com>
2023-07-11 14:30:42 +0000

    ez

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,3 @@ def write_file(filename="", text=""):
     """this is the func"""
     with open(filename, mode='w', encoding='utf-8') as file:
         return file.write(text)
-
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this if func file"""
+
+
+def append_write(filename="", text=""):
+    """this is func"""
+    with open(filename, mode='a', encoding='utf-8') as file:
+       return file.write(text) 
