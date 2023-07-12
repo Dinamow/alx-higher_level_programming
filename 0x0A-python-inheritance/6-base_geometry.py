ea01d6f65b52c4d2f4e520bb3b63847b5e6fd372
DINAMOW <meemoo102039@gmail.com>
2023-07-12 12:23:58 +0000

    ez

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is calss file"""
+
+
+class BaseGeometry:
+    """this is the calss"""
+    def area(self):
+        raise Exception('area() is not implemented')
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-is_same_class = __import__('2-is_same_class').is_same_class
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+bg = BaseGeometry()
 
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
