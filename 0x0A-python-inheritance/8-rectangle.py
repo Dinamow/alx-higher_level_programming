c2c0d5cd5672e93291b3d36e098dd994d572375f
DINAMOW <meemoo102039@gmail.com>
2023-07-12 13:39:34 +0000

    ez

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
 """this is calss file"""
+BaseGeometry = __import__ ('7-base_geometry.py').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     """this if the calss"""
     def __init__(self, width, height):
-        if type(width) is not int:
-            raise TypeError('width must me integer')
-        if width < 0:
-            raise ValueError('width must be positive integer')
-        if type(height) is not int:
-            raise TypeError('height must me integer')
-        if height < 0:
-            raise ValueError('height must be positive integer')
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
