3f7cfe2fc97468ba11c9c2553f3c577963af14bd
DINAMOW <meemoo102039@gmail.com>
2023-07-17 17:18:06 +0000

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,9 +6,43 @@ from models.base import Base
 class Rectangle(Base):
     """this is the calss file"""
     __nb_num = 0
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        if id == None:
+        if id is None:
             type(self).__nb_num += 1
             self.id = type(self).__nb_num
         else:
             self.id = id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        @property
+        def width(self):
+            return self.__width
+
+        def height(self):
+            return self.__height
+
+        def x(self):
+            return self.__x
+
+        def y(self):
+            return self.__y
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+
+        @height.setter
+        def height(self, value):
+            self.__height = value
+
+        @x.setter
+        def x(self, value):
+            self.__x = value
+
+        @y.setter
+        def y(self, value):
+            self.__y = value
