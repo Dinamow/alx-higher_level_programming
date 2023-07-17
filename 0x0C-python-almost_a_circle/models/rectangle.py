7e656060aac75210af56d73438b677d42912eeb3
DINAMOW <meemoo102039@gmail.com>
2023-07-17 17:35:23 +0000

    handle errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,22 @@ class Rectangle(Base):
             self.id = type(self).__nb_num
         else:
             self.id = id
+        if type(width) is not int:
+            raise TypeError('width must be integer')
+        if type(height) is not int:
+            raise TypeError('height must be integer')
+        if type(x) is not int:
+            raise TypeError('x must be integer')
+        if type(y) is not int:
+            raise TypeError('y must be integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
+        if height <= 0:
+            raise TypeError('height must be > 0')
+        if x < 0:
+            raise TypeError('x must be bigger or equal to 0')
+        if y < 0:
+            raise TypeError('y must be bigger or equal to 0')
         self.__width = width
         self.__height = height
         self.__x = x
