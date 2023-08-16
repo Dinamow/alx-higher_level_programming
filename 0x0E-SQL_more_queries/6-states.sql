db706496771979c61768ee07b11f411d5a00eb78
DINAMOW <meemoo102039@gmail.com>
2023-08-16 15:02:11 +0000

    test

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,3 @@
 -- a script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KET, name VARCHAR(256) NOT NULL);
