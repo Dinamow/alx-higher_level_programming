913239ccc7193482ddcecb48a5e99f549c98d808
DINAMOW <meemoo102039@gmail.com>
2023-08-16 15:15:09 +0000

    not null

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	state_id INT,
+	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
