631ff76102de683b7a310dab73cd7993d2766fb0
DINAMOW <meemoo102039@gmail.com>
2023-08-16 15:14:27 +0000

    test

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,1 +1,8 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	state_id INT,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
