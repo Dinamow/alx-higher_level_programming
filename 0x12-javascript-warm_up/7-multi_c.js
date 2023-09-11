19929e0018e15d72d49bfcd6e0821aecc63b2af9
Dinamow <meemoo102039@gmail.com>
2023-09-11 13:07:17 +0000

    fix semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ const args = process.argv;
 
 if (isNaN(parseInt(args[2]))) {
   console.log('Missing number of occurrences');
-} else if (parseInt(args[2]) > 0){
+} else if (parseInt(args[2]) > 0) {
   for (let i = 0; i < parseInt(args[2]); i++) {
-     console.log('C is fun');
+    console.log('C is fun');
   }
 }
