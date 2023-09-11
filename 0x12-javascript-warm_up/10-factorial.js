615cd01758841c3fdf6a76ee03b9e9cffd510a64
Dinamow <meemoo102039@gmail.com>
2023-09-11 13:44:42 +0000

    fix semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,11 +6,11 @@ const fac = (x) => {
   if (x <= 1) {
     return 1;
   } else {
-    return x * fac (x - 1);
+    return x * fac(x - 1);
   }
 };
 if (isNaN(parseInt(args[2]))) {
   console.log('1');
 } else {
-  console.log(fac(parseInt(args[2])))
+  console.log(fac(parseInt(args[2])));
 }
