1e7a45ad9a4758fefab7fe69df2d9e72a72de69e
Dinamow <meemoo102039@gmail.com>
2023-09-11 13:06:04 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (isNaN(parseInt(args[2]))) {
+  console.log('Missing number of occurrences');
+} else if (parseInt(args[2]) > 0){
+  for (let i = 0; i < parseInt(args[2]); i++) {
+     console.log('C is fun');
+  }
+}
