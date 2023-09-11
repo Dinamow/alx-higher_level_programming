7df81d6c46fb7b40f2f4c16d7ca879fb0462664c
Dinamow <meemoo102039@gmail.com>
2023-09-11 13:48:19 +0000

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,14 +3,14 @@
 const args = process.argv;
 
 const fac = (x) => {
+  if (isNaN(parseInt(x))) {
+    return 1;
+  }
   if (x <= 1) {
     return 1;
   } else {
     return x * fac(x - 1);
   }
 };
-if (isNaN(parseInt(args[2]))) {
-  console.log('1');
-} else {
-  console.log(fac(parseInt(args[2])));
-}
+
+console.log(fac(parseInt(args[2])));
