c94d8f4c6a4cf73a39a17ccfa15b23ec61091297
DINAMOW <meemoo102039@gmail.com>
2023-09-14 17:42:43 +0300

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""this is the file"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+
+    if len(args) < 4:
+        exit
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+            user=args[1], password=args[2], db=args[3])
+
+    cursor = db_connection.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    m = cursor.fetchone()
+
+    for i in m:
+        print(i)
+
+    db_connection.close()
+
