78f071ec8e3db6ade111e319dde8f1ed79785b7f
DINAMOW <meemoo102039@gmail.com>
2023-09-14 18:12:22 +0300

    ez

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""this is the file"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    a = args[4]
+    s = f"WHERE name = '{a}'"
+    if len(args) < 4:
+        exit
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                    user=args[1], password=args[2], db=args[3])
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states {:s}".format(s))
+
+    m = cursor.fetchall()
+
+    for i in m:
+        print(i)
+
+    db_connection.close()
