1587504a542f72b1996926170c7ba062e7d1e819
DINAMOW <meemoo102039@gmail.com>
2023-09-15 12:50:10 +0300

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""this is the file"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                    user=args[1], password="password", db=args[3])
+
+    cursor = db_connection.cursor()
+    a = f"SELECT states.id FROM states WHERE states.name = '{args[4]}'"
+    cursor.execute(f"SELECT name FROM cities WHERE cities.state_id = ({a});")
+
+    m = cursor.fetchall()
+
+    for i in m:
+        print(f"{i[0]}", end="\n" if m[-1][0] == i[0] else ", ")
+    
+    db_connection.close()
