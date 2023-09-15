9fe85f0cabd4101683d7b432afc5e194d3213513
DINAMOW <meemoo102039@gmail.com>
2023-09-15 11:35:20 +0300

    s

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     s = f"WHERE name = '{a}'"
 
     db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                    user=args[1], password="password", db=args[3])
+                                    user=args[1], password=args[2], db=args[3])
 
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM states {:s}".format(s))
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     m = cursor.fetchall()
 
     for i in m:
-        print("{:s}".format(i))
+        print(f"{i}")
     db_connection.close()
