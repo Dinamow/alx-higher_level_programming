9de6d7b39ca82695b71ee5f2109785be6f75f2c4
DINAMOW <meemoo102039@gmail.com>
2023-09-17 13:13:13 +0300

    some fixs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     args = sys.argv
     a = args[4]
-    s = f"WHERE name = '{a}' ORDER BY id ASC"
+    s = f"WHERE name LIKE BINARY '{a}' ORDER BY id ASC"
 
     db_connection = MySQLdb.connect(host="localhost", port=3306,
                                     user=args[1], password=args[2], db=args[3])
