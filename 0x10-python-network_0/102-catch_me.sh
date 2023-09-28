e781e4041da6fcfdceafae26abd0604583d95b76
DINAMOW <meemoo102039@gmail.com>
2023-09-28 18:48:53 +0300

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON POST request, and displays the body of the response.
-curl -s -X POST -H "Content-Type: application/json" -d @$2 $1
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
