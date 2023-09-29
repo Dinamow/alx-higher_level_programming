7afaf30289402bdce02ed8b5c04a806e7a9e8a1a
Dinamow <meemoo102039@gmail.com>
2023-09-29 10:07:08 +0300

    .sort()

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,8 +9,6 @@ def find_peak(list_of_integers):
         len(list_of_integers) == 0 or\
             not isinstance(list_of_integers, list):
         return None
+    list_of_integers.sort()
 
-    x = 0
-    for i in list_of_integers:
-        x = i if i > x else x
-    return x
+    return list_of_integers[-1]
