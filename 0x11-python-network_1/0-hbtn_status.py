0f7b407c086a76d47450a87bd096403b567fbbe6
Dinamow <meemoo102039@gmail.com>
2023-09-29 22:17:05 +0300

    .format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,6 +12,6 @@ with request.urlopen(req) as response:
     body = response.read()
 
 print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", body.decode('utf-8'))
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
