ad960904cb24935e3adcea54c51515f85cc23f99
DINAMOW <meemoo102039@gmail.com>
2023-09-29 23:51:16 +0300

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""X-Request-Id
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+
+    print("Response body:", response.text)
