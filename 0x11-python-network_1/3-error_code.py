13df4cd97ce73d1517ec79366424b9fc4016d936
DINAMOW <meemoo102039@gmail.com>
2023-09-29 23:33:47 +0300

    forgot to save

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,15 +4,14 @@
 
 
 if __name__ == "__main__":
-    from urllib import request, parse
+    from urllib import request, error
     import sys
 
     url = sys.argv[1]
-    email = sys.argv[2]
 
-    data = parse.urlencode({'email': email}).encode('utf-8')
-    req = request.Request(url, data=data, method='POST')
-
-    with request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
