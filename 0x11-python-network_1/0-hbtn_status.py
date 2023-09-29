b39cc5354fa00c9191d67dfbb3b76e61115d9869
Dinamow <meemoo102039@gmail.com>
2023-09-29 22:24:35 +0300

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
 
-if __name__ == '__main__':
-    from urllib import request, response
 
-    url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    import urllib.request
 
-    req = request.Request(url)
-
-    with request.urlopen(req) as response:
-        body = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
