9ca4be286b551f45d64465951f2c640c3e70ae4b
DINAMOW <meemoo102039@gmail.com>
2023-10-01 17:17:43 +0300

    API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this is the api"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data["id"]
+        print(user_id)
+    else:
+        print(f"Error: {response.status_code}")
