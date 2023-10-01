6ec9ae39a0beaf14bb1c48a85ee4d35eb7316c65
DINAMOW <meemoo102039@gmail.com>
2023-10-01 18:19:18 +0300

    test

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,20 +6,17 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    if len(sys.argv):
-        print("None")
-        exit()
-
     username = sys.argv[1]
     password = sys.argv[2]
     url = f"https://api.github.com/users/{username}"
-    response = requests.get(url, auth=(username, password))
+    head = {'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': f"Bearer  {password}"}
+    response = requests.get(url, headers=head)
     if response.status_code == 200:
         user_data = response.json()
         user_id = user_data["id"]
         if user_id:
             print(user_id)
-        else:
-            print("None")
     else:
         print("None")
